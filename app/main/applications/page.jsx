64d500a3c558c156157a23e01e7b00e58975e01d
NotMatta <xlenderboy666@gmail.com>
2025-01-21 11:20:31 +0100

    🏃 ready to mingle

--- app/main/applications/page.jsx
+++ app/main/applications/page.jsx
@@ -19,12 +19,12 @@ const AppsPage = () => {
         <h1>Applications</h1>
         <AddApp/>
       </div>
-      <div className="w-full overflow-scroll grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2">
+      <div className="w-full overflow-y-scroll grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2">
         {applications && applications.map((application) => (
           <div key={application.id} className="p-4 border rounded-xl flex flex-col justify-between aspect-square">
             <h2 className="line-clamp-1 text-2xl flex gap-2 items-center"><img src={application.logo} className="h-12 w-12"/>{application.name}</h2>
             <div className="flex-grow text-sm">
-              <h4>{application.address}/{application.login}</h4>
+              <h4>{application.address}<br/>{application.login}</h4>
               <p>Created at {date(application.createdAt)}<br/>
               Last Updated {age(application.updatedAt)} days ago</p>
             </div>
